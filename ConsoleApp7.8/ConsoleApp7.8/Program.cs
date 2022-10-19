b7581527aeb86c4f05b6bf4fce25f5e888d6bf6f
Artur Israilov <kirito0876@gmail.com>
2022-10-19 23:29:28 +0300

    add example 7.8

---
+++ ConsoleApp7.8/ConsoleApp7.8/Program.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleApp7._8;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Repository repository = new Repository();
+        repository.CreateFileName();
+
+       bool menu = true;
+       repository.ShowText();
+
+       while (menu)
+       {
+           switch (Console.ReadLine())
+           {
+               case "1":
+                   Console.Clear();
+                   repository.ShowAllLines();
+                   repository.ShowText();
+                   break;
+               case "2":
+                   Console.Clear();
+                   repository.FileWriter();
+                   repository.ShowText();
+                   break;
+                case "3":
+                   Console.Clear();
+                   Console.Write("Введите ID = ");
+                   repository.FileReaderByKey(int.Parse(Console.ReadLine()));
+                   repository.ShowText();
+                   break;
+                case "4":
+                    Console.Clear();
+                    repository.OrderByWorker();
+                    repository.ShowText();
+                    break;
+                case "5":
+                    Console.Clear();
+                    Console.Write("Введите ID удаляемого сотрудника = ");
+                    repository.DelitedLineByKey(int.Parse(Console.ReadLine()));
+                    repository.ShowText();
+                    break;
+                case "6":
+                    Console.Clear();
+                    repository.ShowLineDate();
+                    repository.ShowText();
+                    break;
+                default:
+                   menu = false;
+                   Console.WriteLine("Завершение работы...");
+                   break;
+           }
+       }
+    }
+}
